04b6fec3d71acaafe75faf5689fa395b0b592ddc
Tchinda Randy <tchindarandy41@gmail.com>
2026-05-21 15:49:34 -0700

    commit made

--- apps/mobile/src/context/ReviewRefreshContext.tsx
+++ apps/mobile/src/context/ReviewRefreshContext.tsx
@@ -1,1 +1,8 @@
 import React, { createContext, useContext, useMemo, useState } from "react";
+
+type ReviewRefreshContextValue = {
+  refreshToken: number;
+  markReviewsUpdated: () => void;
+};
+
+const ReviewRefreshContext = createContext<ReviewRefreshContextValue | undefined>(undefined);
