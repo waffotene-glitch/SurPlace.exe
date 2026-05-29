b46ae9f098f70cf8f23fa27a819946f25879247e
owengaby80 <owengaby80@gmail.com>
2026-05-29 20:19:48 +0100

    upload complete plateController file

--- apps/api/src/controllers/plateController.js
+++ apps/api/src/controllers/plateController.js
@@ -1,2 +1,77 @@
 const asyncHandler = require("express-async-handler");
 const plateService = require("../services/PlateService");
+
+const listPlates = asyncHandler(async (req, res) => {
+  const result = await plateService.listPlates(req.query);
+  res.json(result);
+});
+
+const getPlateDetails = asyncHandler(async (req, res) => {
+  try {
+    const result = await plateService.getPlateDetails(req.params.plateId);
+    res.json(result);
+  } catch (error) {
+    if (error.statusCode) {
+      res.status(error.statusCode);
+    }
+
+    throw error;
+  }
+});
+
+const listManagedPlates = asyncHandler(async (req, res) => {
+  const result = await plateService.listManagedPlates(req.managedRestaurant);
+  res.json(result);
+});
+
+const createManagedPlate = asyncHandler(async (req, res) => {
+  try {
+    const result = await plateService.createManagedPlate(req.managedRestaurant, req.body);
+    res.status(result.statusCode).json(result.body);
+  } catch (error) {
+    if (error.statusCode) {
+      res.status(error.statusCode);
+    }
+
+    throw error;
+  }
+});
+
+const updateManagedPlate = asyncHandler(async (req, res) => {
+  try {
+    const result = await plateService.updateManagedPlate(
+      req.managedRestaurant,
+      req.params.plateId,
+      req.body
+    );
+    res.json(result);
+  } catch (error) {
+    if (error.statusCode) {
+      res.status(error.statusCode);
+    }
+
+    throw error;
+  }
+});
+
+const deleteManagedPlate = asyncHandler(async (req, res) => {
+  try {
+    const result = await plateService.deleteManagedPlate(req.managedRestaurant, req.params.plateId);
+    res.json(result);
+  } catch (error) {
+    if (error.statusCode) {
+      res.status(error.statusCode);
+    }
+
+    throw error;
+  }
+});
+
+module.exports = {
+  listPlates,
+  getPlateDetails,
+  listManagedPlates,
+  createManagedPlate,
+  updateManagedPlate,
+  deleteManagedPlate,
+};
