0ad10777d4ea1ebfffd67e082d1b65c669bad60f
owengaby80 <owengaby80@gmail.com>
2026-05-29 15:37:51 +0100

    set API points for authRouter

--- apps/api/src/routes/authRoutes.js
+++ apps/api/src/routes/authRoutes.js
@@ -3,3 +3,10 @@ const { login, me, register } = require("../controllers/authController");
 const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.get("/me", protect, me);
+
+module.exports = router;
+
