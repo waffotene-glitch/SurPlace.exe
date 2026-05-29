56ba6d3c6904ba860136b51adb935fac7380b27c
owengaby80 <owengaby80@gmail.com>
2026-05-29 20:25:10 +0100

    upload complete userController file

--- apps/api/src/controllers/userController.js
+++ apps/api/src/controllers/userController.js
@@ -1,2 +1,9 @@
 const asyncHandler = require("express-async-handler");
 const userService = require("../services/UserService");
+
+const getProfile = asyncHandler(async (req, res) => {
+  const result = await userService.getProfile(req.user);
+  res.json(result);
+});
+
+module.exports = { getProfile };
