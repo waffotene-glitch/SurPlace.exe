2929967bddb348254510b1ecf1ea86af1c9a97a9
GitWizardBouyem <bouyemlucolivier@gmail.com>
2026-06-01 16:10:14 +0200

    Improved

--- apps/api/src/controllers/managerController.js
+++ apps/api/src/controllers/managerController.js
@@ -1,2 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const managerService = require("../services/ManagerService");
+const getDashboard = asyncHandler(async (req, res) => {
+  const result = await managerService.getDashboard(req.managedRestaurant);
+  res.json(result);
+});
