7523563e218c275394e8e98a865b56e04f0caab5
GitWizardBouyem <bouyemlucolivier@gmail.com>
2026-06-01 16:26:03 +0200

    User controller modifications

---
+++ apps/api/src/controllers/userController.js
@@ -0,0 +1,7 @@
+const asyncHandler = require("express-async-handler");
+const userService = require("../services/UserService");
+
+const getProfile = asyncHandler(async (req, res) => {
+  const result = await userService.getProfile(req.user);
+  res.json(result);
+});
