9fff2f2807405ea303f5344bd4dc7799d86565e2
GitWizardBouyem <bouyemlucolivier@gmail.com>
2026-06-01 16:13:50 +0200

    Modified

--- apps/api/src/controllers/plateController.js
+++ apps/api/src/controllers/plateController.js
@@ -1,2 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const plateService = require("../services/PlateService");
+const listPlates = asyncHandler(async (req, res) => {
+  const result = await plateService.listPlates(req.query);
+  res.json(result);
+});
