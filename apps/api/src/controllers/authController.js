8697f25669b79c9a144883d5d5599270ece1c3b1
GitWizardBouyem <bouyemlucolivier@gmail.com>
2026-06-01 16:04:51 +0200

    improved

--- apps/api/src/controllers/authController.js
+++ apps/api/src/controllers/authController.js
@@ -1,2 +1,10 @@
 const asyncHandler = require("express-async-handler");
 const authService = require("../services/AuthService");
+const register = asyncHandler(async (req, res) => {
+  let user;
+  try {
+    user = await authService.register(req.body);
+  } catch (error) {
+    if (error.statusCode) {
+      res.status(error.statusCode);
+    }
