086f9e9b81dfa3799d0e69fea2b3934666bb66f0
GitWizardBouyem <bouyemlucolivier@gmail.com>
2026-06-01 16:19:55 +0200

    Modified

--- apps/api/src/controllers/restaurantController.js
+++ apps/api/src/controllers/restaurantController.js
@@ -5,3 +5,12 @@ const listRestaurants = asyncHandler(async (req, res) => {
   const result = await restaurantService.listRestaurants(req.query);
   res.json(result);
 });
+const getRestaurantDetails = asyncHandler(async (req, res) => {
+  try {
+    const result = await restaurantService.getRestaurantDetails(req.params.restaurantId);
+    res.json(result);
+  } catch (error) {
+    if (error.statusCode) {
+      res.status(error.statusCode);
+    }
+    
